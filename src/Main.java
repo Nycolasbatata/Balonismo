cf9a91437cedf34e0eb1649b353d25a558f6d7c4
Nicolas Ozórios <nico1228las@gmail.com>
2023-06-15 12:29:37 -0300

    alterações 15/06

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,31 @@ public class Main {
         // Criar objetos do passeio
 
     Pessoa p1 = new Pessoa();
-    Pessoa p2; // declaração do objeto
+    Pessoa p2;
+    Pessoa p3;
+    p3 = new Pessoa(); // declaração do objeto
     p2 = new Pessoa(); // Construção
 
-    //...
+
+
+        //...
     p1.nome = "Elliot Page";
+    p1.cep = "06414-030";
+    p1.rg = "22.222.222-2";
+    p1.cpf = "920.424.234-56";
+    p1.tel = "(11) 98765-8747";
+    p1.telfami = "(11) 96969-6969";
+
+
+    p2.nome = "Daniel Alve";
+    p2.cep = "65645-230";
+    p2.rg = "99.999.999-9";
+    p2.cpf = "945.434.278-86";
+    p2.tel = "(11) 98435-2427";
+    p2.telfami = "(11) 99999-6969";
+
+        p3.nome = "Daniel Alve";
+        p3.cpf = "945.434.278-86";
+        p3.proAnac = "431441322";
 }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -3,7 +3,18 @@ public class Pessoa {
     //Atributos
 
     String nome;
-    
+    String cep;
+    String cpf; //111.111.111-11
+    String rg;
+    String tel;
+    String telfami;
+
+    String proAnac;
+
+    boolean termodecontrato;
+
+
+
     //Métodos
     public void pesquisarEmpresa(){
 
